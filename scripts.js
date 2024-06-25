2ca3145c1a73005185ab5f300f56d82db323cec3
Kiarash <163787116+kiarashlotfalizadeh@users.noreply.github.com>
2024-06-25 18:25:53 -0400

    Complete playRound function in script file
    Ccreated playRound function which takes both choices and outputs score

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 function getComputerChoice () {
-  randomNumber = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  let computerChoice;
 
   switch (randomNumber) {
     case 0:
@@ -16,7 +18,54 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-  humanChoice = prompt("Rock, Paper, Scissors?");
+  let humanChoice = prompt("Rock, Paper, Scissors?");
   return humanChoice.toLowerCase();
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("You tied!");
+  } 
+
+  switch (humanChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case "paper":
+          console.log("You lose! Paper beats Rock");
+          computerScore++;
+          break;
+        case "scissors":
+          console.log("You win! Rock beats Scissors");
+          humanScore++;
+          break;
+      }
+    break;
+    case "paper":
+      switch (computerChoice) {
+        case "rock":
+          console.log("You win! Paper beats Rock");
+          humanScore++;
+          break;
+        case "scissors":
+          console.log("You lose! Scissors beats Paper");
+          computerScore++;
+          break;
+      }
+    break;
+    case "scissors":
+      switch (computerChoice) {
+        case "rock":
+          console.log("You lose! Rock beats Scissors");
+          computerScore++;
+          break;
+        case "paper":
+          console.log("You win! Scissors beats Paper");
+          humanScore++;
+          break;
+      }
+    break;
+  }
+}
